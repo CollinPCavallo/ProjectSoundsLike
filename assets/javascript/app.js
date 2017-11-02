af67bacd688e0c63cae81348763ef608f9c74c39
Collin Cavallo <collincavallo@gmail.com>
2017-11-01 21:29:55 -0500

    added javascript and css

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".buttonLeft").click(function(){
+        $("#search").hide()
+        $("#mail").hide()
+        $("#about").hide()
+        console.log("Search Clikced")
+    
+    })
+    $(".buttonRight").click(function(){
+        $("#search").hide()
+        $("#mail").hide()
+        $("#about").hide()
+        console.log("Search Clikced")
+    
+    })
+    $(".buttonMid").click(function(){
+        $("#search").hide()
+        $("#mail").hide()
+        $("#about").hide()
+        console.log("Search Clikced")
+    
+    })
+
+})
+
+
