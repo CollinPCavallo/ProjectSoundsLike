541cf3be148e68e166f51e5793ade5a317674856
Collin Cavallo <collincavallo@gmail.com>
2017-11-04 11:52:00 -0500

    added first api working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,19 +13,46 @@ $(document).ready(function () {
         console.log("Search Clikced")
 
     })
-    $(".buttonMid").click(function () {
+    $(".buttonMid").click(function (event) {
+        event.preventDefault();
+        
         $("#search").hide()
         $("#mail").hide()
         $("#about").hide()
         console.log("Search Clikced")
+        
+        var searchBar = $("<input>")
+        var submitBtn = $("<button>")
+        
+        searchBar.addClass("search");
 
+        searchBar.attr("type", "text")
+        searchBar.attr("id", "artistInput")
+        searchBar.attr("placeholder", "Artist Name")
+
+        submitBtn.attr("id", "subButn")
+        submitBtn.attr("type", "submit")
+
+        
+        $("#searchForm").append(searchBar);
+        $("#searchForm").append(submitBtn);
+
+        
     })
+    
+    $("#searchForm").submit(function(e){
+
+        e.preventDefault();
+        var userInput = $("#artistInput").val()
+        console.log(userInput)
+
+    
 
     var rapid = new RapidAPI("default-application_59fbc461e4b06d2e9b6cef94", "e938f07c-2598-49e9-adbf-79f15a8f44d0");
 
     rapid.call('LastFM', 'getSimilarArtists', {
         'apiKey': '1a2f69cf8d628ef504422e08d206f368',
-        'artist': 'Datsik',
+        'artist': userInput,
         'limit': '5',
         'autocorrect': 'true'
 
@@ -35,5 +62,9 @@ $(document).ready(function () {
     }).on('error', function (payload) {
         /*YOUR CODE GOES HERE*/
     });
+    console.log(userInput)
+    
+})
+    
 
 })
