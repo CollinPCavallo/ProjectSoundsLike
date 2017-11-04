b2bba54343c07a6b5e878607a21e59012c70d1a9
Collin Cavallo <collincavallo@gmail.com>
2017-11-04 09:59:11 -0500

    added first api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,39 @@
-$(document).ready(function(){
-    $(".buttonLeft").click(function(){
+$(document).ready(function () {
+    $(".buttonLeft").click(function () {
         $("#search").hide()
         $("#mail").hide()
         $("#about").hide()
         console.log("Search Clikced")
-    
+
     })
-    $(".buttonRight").click(function(){
+    $(".buttonRight").click(function () {
         $("#search").hide()
         $("#mail").hide()
         $("#about").hide()
         console.log("Search Clikced")
-    
+
     })
-    $(".buttonMid").click(function(){
+    $(".buttonMid").click(function () {
         $("#search").hide()
         $("#mail").hide()
         $("#about").hide()
         console.log("Search Clikced")
-    
+
     })
+
+    var rapid = new RapidAPI("default-application_59fbc461e4b06d2e9b6cef94", "e938f07c-2598-49e9-adbf-79f15a8f44d0");
+
+    rapid.call('LastFM', 'getSimilarArtists', {
+        'apiKey': '1a2f69cf8d628ef504422e08d206f368',
+        'artist': 'Datsik',
+        'limit': '5',
+        'autocorrect': 'true'
+
+    }).on('success', function (payload) {
+        console.log(payload)
+        /*YOUR CODE GOES HERE*/
+    }).on('error', function (payload) {
+        /*YOUR CODE GOES HERE*/
+    });
 
 })
-
-
