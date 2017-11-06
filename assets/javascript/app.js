9b61285da659b60fb1b6df7c8c42d4934960c9d3
Collin Cavallo <collincavallo@gmail.com>
2017-11-06 11:20:45 -0600

    Finished Adding Second Api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,223 @@
 $(document).ready(function () {
+    
     $(".buttonLeft").click(function () {
+    
         $("#search").hide()
+    
         $("#mail").hide()
+    
         $("#about").hide()
+    
         console.log("Search Clikced")
 
     })
+    
     $(".buttonRight").click(function () {
+    
         $("#search").hide()
+    
         $("#mail").hide()
+    
         $("#about").hide()
+    
         console.log("Search Clikced")
 
     })
+    
     $(".buttonMid").click(function () {
-        
 
         $("#search").hide()
+
         $("#mail").hide()
+
         $("#about").hide()
+
         console.log("Search Clikced")
 
         var searchBar = $("<input>")
+
         var submitBtn = $("<button>")
+
         var resetButton = $("<button>")
 
         resetButton.addClass("resetHome");
+
         resetButton.attr("type", "button")
 
         resetButton.html("X");
 
-
         searchBar.addClass("search");
 
         searchBar.attr("type", "text")
+
         searchBar.attr("id", "artistInput")
+
         searchBar.attr("placeholder", "Artist Name")
 
         submitBtn.attr("id", "subButn")
+
         submitBtn.attr("type", "submit")
 
-
         $("#searchForm").append(searchBar);
+
         $("#searchForm").append(submitBtn);
+
         $("#reset").append(resetButton);
-        
-
-
-
-
-
 
     })
 
     $("#searchForm").submit(function (e) {
 
         e.preventDefault();
+
         var userInput = $("#artistInput").val()
+
         console.log(userInput)
+
         $("#artist-list").empty();
+        $("#preview-player").empty();
+
         $("#artistInput").val("");
-
-
 
         var rapid = new RapidAPI("default-application_59fbc461e4b06d2e9b6cef94", "e938f07c-2598-49e9-adbf-79f15a8f44d0");
 
         rapid.call('LastFM', 'getSimilarArtists', {
+
             'apiKey': '1a2f69cf8d628ef504422e08d206f368',
+
             'artist': userInput,
+
             'limit': '5',
+
             'autocorrect': 'true'
 
         }).on('success', function (payload) {
 
             console.log(payload)
-            for (var i = 0; i < 5; i++) {
-                var artList = payload.similarartists.artist[i].name
-                var artImgPath = parseInt(payload.similarartists.artist[i].image[2]);
-                var createName = $("<p>")
-                var artistImg = $("<img>")
 
-                artistImg.attr("src", artImgPath)
+            for (var i = 0; i < 5; i++) {
+
+                var artList = payload.similarartists.artist[i].name
+
+                var artLinkPath = payload.similarartists.artist[i].url;
+
+                var createName = $("<p>")
+
+                var artistLink = $("<a>")
 
                 createName.append(artList)
+
                 createName.addClass("artistName")
 
+                createName.attr("newName", payload.similarartists.artist[i].name)
+
+                artistLink.attr("href", artLinkPath);
+
+                artistLink.attr("target", "blank");
+
+                artistLink.append("Check out " + artList + " here!");
+
+                $("#artist-list").attr("style", "border: 2px solid #80cbc4;")
+
+                $("#artistInput").attr("placeholder", "Search Again");
+
                 $("#artist-list").append(createName);
-                $("#artist-list").append(artistImg);
+
+                $("#artist-list").append(artistLink);
 
                 console.log(artList)
+
+
+
             }
+
+            $(".artistName").click(function () {
+
+                var iTunesSearch = $(this).attr("newName");
+
+                var rapid = new RapidAPI("default-application_59fbc461e4b06d2e9b6cef94", "e938f07c-2598-49e9-adbf-79f15a8f44d0");
+
+                var dispName = $("<p>")
+
+                dispName.append(iTunesSearch)
+
+                $("#preview-player").append(dispName);
+
+                $("#preview-player").attr("style", "border: 2px solid #80cbc4;")
+
+                rapid.call('iTunes', 'searchMusic', {
+
+                    'term': iTunesSearch,
+
+                    'country': 'US',
+
+                    'entity': 'musicTrack',
+
+                    'limit': '3',
+
+                    'lang': 'en_us'
+
+                }).on('success', function (musicData) {
+
+                    console.log(musicData);
+
+                    for (var k = 0; k < 3; k++) {
+
+                        var previewUrl = musicData.results[k].previewUrl
+
+                        var trackName = musicData.results[k].trackName
+
+                        var createPreview = $("<p>")
+
+                        createPreview.addClass("iframeText");
+
+                        createPreview.append("Check Out " + iTunesSearch + "'s song " + "'" + trackName + "'" + " here");
+
+                        createPreview.attr("data-link", previewUrl)
+
+                        createPreview.attr("song", trackName)
+
+                        $("#preview-player").append(createPreview);
+
+                    }
+
+                    $(".iframeText").click(function () {
+
+                        var createDiv = $("<div>")
+
+                        createDiv.addClass("previewSong")
+
+                        $(".previewSong").html("")
+
+                        var createPlayer = $("<iframe>")
+
+                        createPlayer.attr("src", $(this).attr("data-link"))
+
+                        createPlayer.attr("width", "450")
+
+                        createPlayer.attr("height", "200")
+
+                        createDiv.append(createPlayer)
+
+                        $(this).append(createDiv)
+                        
+                        $(".artistName").click(function(){
+
+                            $("#preview-player").html("");
+
+                        })
+                    })
+                    /*YOUR CODE GOES HERE*/
+                }).on('error', function (musicData) {
+                    /*YOUR CODE GOES HERE*/
+                });
+
+            })
             /*YOUR CODE GOES HERE*/
         }).on('error', function (payload) {
             /*YOUR CODE GOES HERE*/
         });
-        console.log(userInput)
 
     })
-
 
 })
